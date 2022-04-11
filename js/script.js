ee16e7ee742c0ebed11231c4339247e15e060985
tcut-sci-tech <88429838+tcut-sci-tech@users.noreply.github.com>
2022-04-11 19:48:53 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,5 +8,5 @@
 		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
 		centerMode: true,//要素を中央ぞろえにする
 		variableWidth: true,//幅の違う画像の高さを揃えて表示
-		dots: false,//下部ドットナビゲーションの表示
+		dots: true,//下部ドットナビゲーションの表示
 	});
